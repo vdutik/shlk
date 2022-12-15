4844df550f0478571908878683afd628fd9c6d00
vdutik <32429448+vdutik@users.noreply.github.com>
2022-12-15 22:30:33 +0200

    add sort and translates

---
+++ database/migrations/2022_12_15_153243_add_sort_to_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tags', function (Blueprint $table) {
+
+           $table->unsignedInteger('post_id');
+           $table->unsignedBigInteger('tag_id');
+
+           $table->foreign('post_id')->references('post_id')->on('posts');
+           $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tags');
+    }
+}
