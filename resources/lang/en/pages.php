3a8af6658e2b45bfa98c2984b403686902c6d48c
vdutik <32429448+vdutik@users.noreply.github.com>
2023-04-09 23:44:05 +0300

    add some translation package

---
+++ resources/lang/en/pages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+
+    'title_news' => 'Новини',
+    'title_public_info' => 'Публічна інформація',
+    'title_for_student' => 'Студенту',
+    'title_admission' => 'Абітурієнту',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
