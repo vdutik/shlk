901ace7bd9d074d66e72f4a64f6c54190167118c
vdutik <32429448+vdutik@users.noreply.github.com>
2023-04-19 22:22:44 +0300

    meta initial-scale

--- resources/views/layouts/article_page.blade.php
+++ resources/views/layouts/article_page.blade.php
@@ -3,15 +3,13 @@
 
 <head>
   <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>{{ config('app.name', 'SHLK') }} | {{ isset($article) ? $article : $title }}</title>
   <meta name="description" content="{{__('general.short_name')}} -  Website." />
-
 {{--  <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">--}}
 
   <link href="https://unpkg.com/swiper/swiper-bundle.min.css" rel="stylesheet">
